fe94e4528c9b493590cd07293a06b98b2dbd487a
Bjornir90 <celestincollin@gmail.com>
2019-08-11 14:04:08 +0100

    Arrows travel straight for some time before gravity affects them

--- src/main/java/com/bjornir/terrabound/entities/Arrow.java
+++ src/main/java/com/bjornir/terrabound/entities/Arrow.java
@@ -4,9 +4,15 @@ import com.bjornir.terrabound.utils.Side;
 import com.bjornir.terrabound.utils.Vector;
 
 public class Arrow extends Entity {
+    private static final int millisecondsOfFlyingStraight = 250;
+    private int timeFromCreation;
+    private boolean isLanded;
 
     public Arrow(String spritePath, int width, int height) {
         super(spritePath, width, height);
+        timeFromCreation = 0;
+        isPhysical = false;
+        isLanded = false;
     }
 
 
@@ -14,5 +20,14 @@ public class Arrow extends Entity {
     protected void onTerrainCollision(Side side){
         speed = new Vector();
         isPhysical = false;
+        isLanded = true;
+    }
+
+    @Override
+    public void update(int delta){
+        super.update(delta);
+        timeFromCreation += delta;
+        if(timeFromCreation >= millisecondsOfFlyingStraight && !isLanded)
+            isPhysical = true;
     }
 }
